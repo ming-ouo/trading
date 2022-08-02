8dc903723373826037d4f90677b90b4f24201f1f
Ming <ming@alone.tw>
2022-08-02 14:24:29 +0800

    test: Add TestOrderBook_NewLimitPriceOrder

--- pkg/orderbook/orderbook_test.go
+++ pkg/orderbook/orderbook_test.go
@@ -1,8 +1,30 @@
 package orderbook
 
-import "testing"
+import (
+	"github.com/shopspring/decimal"
+	"testing"
+	"time"
+)
 
 func TestOrderBook_NewLimitPriceOrder(t *testing.T) {
-	//ob := NewOrderBook()
-	//ob.NewLimitPriceOrder()
+	ob := NewOrderBook()
+
+	arr := make([]*Order, 0)
+	for i := int64(0); i < 3; i++ {
+		newOrder := NewOrder("", Sell, 10, time.Now().UnixNano(), 0, decimal.NewFromInt(i+1), decimal.NewFromInt(0))
+		arr = append(arr, newOrder)
+		ob.NewLimitPriceOrder(newOrder)
+	}
+
+	if ob.SellQueue.Size() != 3 {
+		t.Errorf("got %d, want %d", ob.SellQueue.Size(), 3)
+	}
+
+	newOrder := NewOrder("", Buy, 10, time.Now().UnixNano(), 0, decimal.NewFromInt(1), decimal.NewFromInt(0))
+	arr = append(arr, newOrder)
+	ob.NewLimitPriceOrder(newOrder)
+
+	if ob.SellQueue.Size() != 2 {
+		t.Errorf("got %d, want %d", ob.SellQueue.Size(), 2)
+	}
 }
